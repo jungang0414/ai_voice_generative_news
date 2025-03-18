de0936d3dc1ee74a4e22f990f0b1d4602e7866dc
jungang0414 <dayr88520@gmail.com>
2025-03-18 10:47:08 +0800

    :bug: Vercel 無法部屬後端 2

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+// routes
+import ttsRouter from "./routes/tts.js";
+import newsRouter from "./routes/news.js";
+import baseRouter from "./routes/base.js";
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("This API Is Working!"));
+
+app.use("/api/v1", ttsRouter);
+app.use("/api/v1", newsRouter);
+app.use("/api/v1", baseRouter);
+
+app.listen(PORT, () => {
+    console.log(`tts 伺服器已啟動, PORT ${PORT}`);
+});
+
+export default app;
