82b6aa023137a3562c9d4eaadfa0e229fa848ad1
jungang0414 <dayr88520@gmail.com>
2025-03-18 10:12:36 +0800

    :sparkles: 新增部屬Vercel後端所需設定

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import newsRouter from "./routes/news.js";
 import baseRouter from "./routes/base.js";
 
 const app = express();
+const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cors());
@@ -19,3 +20,5 @@ app.use("/api/v1", baseRouter);
 app.listen(PORT, () => {
     console.log(`tts 伺服器已啟動, PORT ${PORT}`);
 });
+
+module.exports = app
